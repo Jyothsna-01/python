229f62c197dae7c5e493a3a42d9cc4384b4d461e
Jyothsna-01 <126751997+Jyothsna-01@users.noreply.github.com>
2023-03-02 12:53:01 +0530

    swaping numbers

---
+++ swap_numbers.py
@@ -0,0 +1,19 @@
+''' program for swaping strings'''
+'''
+a = int(input("enter the value of a:"))
+b = int(input("enter the value of b:"))
+print("before swapping a = {},before swapping b = {}".format(a,b))
+temp = a
+a = b
+b = temp
+print("After swapping a = {},After swapping b = {}".format(a,b))
+'''
+a = int(input("enter the a val:"))
+b = int(input("enter the b val:"))
+a = a+b
+b = a-b
+a= a-b
+print(a,b)
+
+
+
